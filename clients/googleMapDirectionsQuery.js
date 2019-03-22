78eaf3fffd1cae51bb11f71ce8100bd7370074c5
H8JKT032 <you@example.com>
2019-03-22 15:22:01 +0700

    init

--- clients/googleMapDirectionsQuery.js
+++ clients/googleMapDirectionsQuery.js
@@ -3,15 +3,25 @@
 class GoogleMapDirectionsQuery {
 
   constructor(routeOptimizerQuery) {
-    const waypoints = routeOptimizerQuery.tasks.map(function(task) {
+    const waypoints = routeOptimizerQuery.tasks.map(function (task) {
       return {
         lat: task.lat,
         lng: task.lng,
       };
     });
 
+    let tasklen = routeOptimizerQuery.tasks.length;
+    let routingType = routeOptimizerQuery.routingType;
     this.origin = routeOptimizerQuery.home;
-    this.destination = routeOptimizerQuery.home;
+    if (!routingType) {
+      this.destination = routeOptimizerQuery.home;
+    } else if (routingType === 'AtoZ') {
+      this.destination = routeOptimizerQuery.tasks[tasklen - 1];
+    } else if (routingType === 'RoundTrip') {
+      this.destination = routeOptimizerQuery.home;
+    } else {
+      this.destination = routeOptimizerQuery.home;
+    }
     this.waypoints = waypoints;
     this.optimize = true;
     this.mode = 'driving';
